5089a8666d561c9d8919a0c991bbfec0086bed4a
angus-turner <85502368+angus-turner@users.noreply.github.com>
2021-06-22 11:57:35 +0100

    Update index.js
    filter out duplicates when merging arrays

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 'use strict';
+const winston = require('winston');
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.cli(),
+//  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    //new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.Console()
+  ],
+});
 
 const readFileSync = require('fs').readFileSync;
 const jsYaml = require('js-yaml');
@@ -19,9 +34,25 @@ function readAsJSON(fileName) {
  */
 function yamlMerge(...from) {
   const files = from.map((path) => readAsJSON(path));
-  const outputJSON = _.mergeWith({}, ...files, (objValue, srcValue) => {
+  const outputJSON = _.mergeWith(files[0],files[1], (objValue, srcValue) => {
+
+
     if (Array.isArray(objValue) && Array.isArray(srcValue)) {
-      return [...objValue, ...srcValue];
+      logger.info('srcValue')
+      logger.info(srcValue)
+      logger.info('objValue')
+      logger.info(objValue)
+
+        const newArray = srcValue.filter(item => {
+          logger.info(item);
+          logger.info(objValue);
+          logger.info(objValue.indexOf(item)==-1);
+          return objValue.indexOf(item)==-1;
+        })
+
+        logger.info(newArray);
+        
+      return [...objValue, ...newArray].sort();
     }
 
     // handle it just like with _.merge
