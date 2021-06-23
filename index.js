e678c1a86d50fe39736d9526d0242cb843deac59
angus.turner <angus.turner@servexternos.gruposantander.com>
2021-06-23 11:05:09 +0100

    convert, select definitions & merge yamls

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
 const winston = require('winston');
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
+    level: 'info',
+    format: winston.format.json(),
 //  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    //new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-    new winston.transports.Console()
-  ],
+    transports: [
+        //
+        // - Write all logs with level `error` and below to `error.log`
+        // - Write all logs with level `info` and below to `combined.log`
+        //
+        //new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({filename: 'combined.log'}),
+        new winston.transports.Console()
+    ],
 });
 
 const readFileSync = require('fs').readFileSync;
@@ -20,35 +20,34 @@ const jsYaml = require('js-yaml');
 const _ = require('lodash');
 
 function readAsJSON(fileName) {
-  const fileBuffer = readFileSync(fileName);
-  const fileString = fileBuffer.toString();
+    logger.info(fileName)
+    const fileBuffer = readFileSync(fileName);
+    const fileString = fileBuffer.toString();
 
-  return jsYaml.load(fileString);
+    return jsYaml.load(fileString);
 }
 
-function cleanupOpenAPI(filename) {
-  const base = readAsJSON(filename);
-  return base.definitions;
+function cleanupOpenAPI(swaggerVersion2) {
+    //TODO: strip out xml references
+    return swaggerVersion2.spec.definitions;
 }
 
 function yamlMerge(doc1, doc2) {
-  const outputJSON = _.mergeWith(files[0],files[1], (objValue, srcValue) => {
+    const outputJSON = _.mergeWith(doc1, doc2, (objValue, srcValue) => {
+        if (Array.isArray(objValue) && Array.isArray(srcValue)) {
 
+            const newArray = srcValue.filter(item => {
+                return objValue.indexOf(item) == -1;
+            })
 
-    if (Array.isArray(objValue) && Array.isArray(srcValue)) {
+            return [...objValue, ...newArray].sort();
+        }
 
-        const newArray = srcValue.filter(item => {
-          return objValue.indexOf(item)==-1;
-        })
+        // handle it just like with _.merge
+        return undefined;
+    });
 
-      return [...objValue, ...newArray].sort();
-    }
-
-    // handle it just like with _.merge
-    return undefined;
-  });
-
-  return jsYaml.dump(outputJSON);
+    return jsYaml.dump(outputJSON);
 }
 
 /**
@@ -58,8 +57,8 @@ function yamlMerge(doc1, doc2) {
  * @return {string} the output YAML file
  */
 function yamlMergeFromPath(...from) {
-  const files = from.map((path) => readAsJSON(path));
-  return yamlMerge(files[0], files[1]);
+    const files = from.map((path) => readAsJSON(path));
+    return yamlMerge(files[0], files[1]);
 }
 
-module.exports = {yamlMerge, cleanupOpenAPI};
+module.exports = {yamlMerge, readAsJSON, cleanupOpenAPI};
